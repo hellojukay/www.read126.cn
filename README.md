d040404a5b7509f58ff2c7f03856d67c78dc03f4
hellojukay <licong@qianxin.com>
2021-12-08 20:47:20 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # www.read126.cn
 爬取 www.read126.cn 的小说
+
+列出所有的小说
+```shell
+perl list.pl http://www.read126.cn/
+```
+下载小说
+```shell
+perl download.pl http://www.read126.cn/a5e5d534-564a-4beb-9a5e-255c182d768c!108fe0a9-f143-4058-96ea-ced860f4fac7!cc4b8c25-bbcc-48d6-9669-dbbd11bda78e.html
+```
---
+++ list.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+use HTTP::Tiny;
+use Encode;
+use HTML::Element;
+use HTML::Query 'Query';
+
+binmode( STDOUT, "encoding(UTF-8)" );
+
+sub get ($) {
+    my $response = HTTP::Tiny->new->get(shift);
+    die "Failed!\n" unless $response->{success};
+    return decode( "gb2312", $response->{content} );
+}
+
+# 获取章节列表
+sub get_novels ($) {
+    my $q     = Query( text => shift );
+    my @links = $q->query('td>a[title]')->get_elements();
+    return map {
+        sprintf "%s http://www.read126.cn/%s", $_->as_trimmed_text,
+          $_->attr('href')
+    } @links;
+}
+
+my $url = $ARGV[0];
+unless ($url) {
+    printf "please enter url link\n";
+    exit(1);
+}
+my $text     = get($url);
+my @novels    = get_novels($text);
+foreach my $novel (@novels) {
+    print "$novel\n";
+}
+
